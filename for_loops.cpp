010ea70f5870da69be7ec0cf7bf590f642faedb4
sam-parks15 <samuelparks15@gmail.com>
2025-09-17 17:22:07 +0000

    learn loops

---
+++ for_loops.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int i;
+    int n;
+
+    cout << "Enter n: ";
+    cin >> n;
+
+    for (i=0;i<n;i++) {
+        cout << "i: " << i << endl; // code that will run as long as the for's book condition is true
+    }
+
+    return 0;
+}
